Fixed aspect ratio and L1 norm calculation

extract_comparison.py
@@ -6,7 +6,8 @@ resolutions = [32, 64, 128, 256, 512, 1024, 2048]
 
 results = []
 for N in tqdm(resolutions):
-  Nx = N//4
+  Nx = 4 #N//4
+  dy = 2.0 / N
   
   path_isentropic = f'hse/isentropic_{N}/run.h5'
   f_isentropic = h5py.File(path_isentropic, 'r')
@@ -15,7 +16,7 @@ for N in tqdm(resolutions):
   final_pressure = np.array(f_isentropic[f'ite_{Nite-1:04}/prs'])[:, Nx//2]
 
   diff = init_pressure - final_pressure
-  isentropic_L1 = np.linalg.norm(diff, ord=1.0)
+  isentropic_L1 = np.sum(np.abs(diff)) * dy
   isentropic_max_v = np.max(np.abs(f_isentropic[f'ite_{Nite-1:04}/v']))
 
   path_isothermal = f'hse/isothermal_{N}/run.h5'
@@ -25,7 +26,7 @@ for N in tqdm(resolutions):
   final_pressure = np.array(f_isothermal[f'ite_{Nite-1:04}/prs'])[:, Nx//2]
 
   diff = init_pressure - final_pressure
-  isothermal_L1 = np.linalg.norm(diff, ord=1.0)
+  isothermal_L1 = np.sum(np.abs(diff)) * dy
   isothermal_max_v = np.max(np.abs(f_isothermal[f'ite_{Nite-1:04}/v']))
 
   results.append((N, isentropic_L1, isothermal_L1, isentropic_max_v, isothermal_max_v))